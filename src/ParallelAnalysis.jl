8ebc82c42328f0966b9509e684bf1ffe6f5d28ad
takuizum <sep10.taku.izum@gmail.com>
2021-07-01 20:02:10 +0900

    Add polycoric cor

--- src/ParallelAnalysis.jl
+++ src/ParallelAnalysis.jl
@@ -1,5 +1,16 @@
 module ParallelAnalysis
 
 using MultivariateStats, StatsFuns, Statistics
+# Polychor
+using Trapz: trapz
+using Optim: optimize, Brent
+using LinearAlgebra: LowerTriangular, diagind
+using QuadGK: quadgk
+using Distributions, DataFrames
+
+include("Polychoric.jl")
+export polycor
+
 
 end
+
