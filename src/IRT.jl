f4804acdbf8994cb2cb86d58bbcb34bd3cdd710a
takuizum <sep10.taku.izum@gmail.com>
2021-07-05 01:42:26 +0900

    Add hueristicIRT method

--- src/IRT.jl
+++ src/IRT.jl
@@ -35,3 +35,44 @@ Transform vector to vector of vector
 function v2vv(v)
     return [[i] for i in v]
 end
+
+# Heuristic IRT parameter estimation based on the polychoric correlations.
+
+mutable struct HeuristicIRT
+    a
+    d
+    b
+end
+
+function transform_FA_IRT(α::T, τ::Vector{T}) where T <: Real
+    σ = √(1 - α^2)
+    a = α / σ
+    d = -τ ./ σ
+    b = -d ./ a
+    return a, d[begin+1:end-1], b[begin+1:end-1]
+end
+
+function heuristicIRT(X; method = :em)
+    J = size(X, 2)
+    r = Matrix{Float64}(undef, J, J)
+    r[diagind(r)] .= 1.0
+    τ = Vector{Vector{Float64}}(undef, J)
+    for i in 1:J
+        x = @view X[:, i]
+        for j in i:J
+            y = @view X[:, j]
+            p = polyc(x, y; verbose = false)
+            r[j, i] = p.ρ
+            r[i, j] = r[j, i]
+            if j == J
+                τ[i] = p.τ₁
+            end
+        end
+    end
+    # FA
+    replace_diagonal!(r)
+    f = fit(FactorAnalysis, r; mean = fill(0.0, J), maxoutdim = 1, method = method)
+    α = loadings(f)
+    tp = transform_FA_IRT.(α, τ)
+    return HeuristicIRT(getindex.(tp, 1), getindex.(tp, 2), getindex.(tp, 3))
+end
