0af67775f428515ce179f6b2753aa81ffae93e4f
takuizum <sep10.taku.izum@gmail.com>
2021-07-04 19:58:13 +0900

    Add FA model

--- src/FA.jl
+++ src/FA.jl
@@ -2,6 +2,7 @@
 struct fa
     data
     fit
+    cor
     loadings
     cov
     projection
@@ -17,10 +18,47 @@ Factor analysis wrapper.
 `args` are arguments for FA model. See, `MultivariateStats.fit(FactorAnalysis, X; args...)`(https://multivariatestatsjl.readthedocs.io/en/latest/fa.html).
 `maxoutdim` was preferentially fixed by `nfactors` and `mean` was fixed by 0. User can modified only `method`, `tol`, `tot` and `η`.
 """
-function fa(M, nfactors = 1; args...)
+
+function fa(M; method = :Polychoric, nfactors = 1, args...)
+    if method == :Polychoric
+        fa_polychoric(M, nfactors; args...)
+    elseif method == :Pearson
+        fa_pearson(M, nfactors; args...)
+    else
+        @warn "`method` should be choosen from (:Pearson, :Polychoric)."
+        return nothing
+    end
+end
+
+function fa_polychoric(M, nfactors = 1; args...)
+    S = convert(Matrix{Float64}, polycor(M))
+    n = size(S, 1)
+    ft = fit(FactorAnalysis, S; mean = fill(0.0, n), maxoutdim = nfactors, args...)
+    fa(M, ft, :Polychoric, loadings(ft), cov(ft), projection(ft), nfactors, n)
+end
+function fa_pearson(M, nfactors = 1; args...)
     S = convert(Matrix{Float64}, cor(M))
     n = size(S, 1)
     ft = fit(FactorAnalysis, S; mean = fill(0.0, n), maxoutdim = nfactors, args...)
-    fa(M, ft, loadings(ft), cov(ft), projection(ft), nfactors, n)
+    fa(M, ft, :Pearson, loadings(ft), cov(ft), projection(ft), nfactors, n)
 end
 
+"""
+    cov(x::fa)
+Extract covariance matrix from FA models.
+"""
+function cov(x::fa)
+    x.cov
+end
+
+"""
+    loadings(x::fa)
+Extract factor loadings from FA models.
+"""
+function loadings(x::fa)
+    x.loadings
+end
+
+function show(io::IO, x::fa)
+    println(io, "Factor Analysis $(x.cor)")
+end
