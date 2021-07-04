9cdc9634b8ca81c8895bde35ec4f727ceb8c099a
takuizum <sep10.taku.izum@gmail.com>
2021-07-04 19:59:37 +0900

    Update PA

--- src/ParallelAnalysis.jl
+++ src/ParallelAnalysis.jl
@@ -1,16 +1,34 @@
 module ParallelAnalysis
 
 using MultivariateStats, StatsFuns, Statistics
-# Polychor
+import Statistics: cov
+import MultivariateStats: loadings
+import Base: show
+# Polychoric
 using Trapz: trapz
 using Optim: optimize, Brent
 using LinearAlgebra: LowerTriangular, diagind
 using QuadGK: quadgk
 using Distributions, DataFrames
+# Plot recipes
+using RecipesBase
 
 include("Polychoric.jl")
-export polycor
+export polycor, cov, loadings
 
+include("RandomMatrix.jl")
+export random_matrix
+
+include("FA.jl")
+export fa
+
+include("IRT.jl")
+export generate_response, v2vv
+
+include("PA.jl")
+export parallel
+
+export show
 
 end
 
