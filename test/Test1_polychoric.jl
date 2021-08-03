3030f11a415bc3bd0b936b0e9581999b919ca0f0
takuizum <sep10.taku.izum@gmail.com>
2021-08-04 03:48:23 +0900

    Update test

--- test/Test1_polychoric.jl
+++ test/Test1_polychoric.jl
@@ -1,33 +1,49 @@
 using Distributions, ParallelAnalysis
 # Support function
-function discretize(x, th)
-    sum(th .< x)
-end
-# Gen sim data.
-raw = rand(MvNormal([0, 0], [1 .5; .5 1]), 10000)'
-τ1 = sort!(rand(Uniform(-2, 2), 3))
-τ2 = sort!(rand(Uniform(-2, 2), 4))
-obs = [discretize.(raw[:, 1], Ref(τ1)) discretize.(raw[:, 2], Ref(τ2)) ]
 
 @testset "polycor" begin
+    function discretize(x, th)
+        sum(th .< x)
+    end
+    # Gen sim data.
+    raw = rand(MvNormal([0, 0], [1 .5; .5 1]), 10000)'
+    τ1 = sort!(rand(Uniform(-2, 2), 3))
+    τ2 = sort!(rand(Uniform(-2, 2), 4))
+    obs = [discretize.(raw[:, 1], Ref(τ1)) discretize.(raw[:, 2], Ref(τ2)) ]
+
     @test polycor(obs) isa Matrix
+    ctb = ParallelAnalysis.contab(obs[:, 1], obs[:, 2])
+    @test isapprox(sum(ctb.X, dims = 2), ctb.mx)
+    @test isapprox(sum(ctb.X, dims = 1)[:], ctb.my)
+    plc = ParallelAnalysis.polyc(ctb)
+    @test 0 ≤ plc.ρ ≤ 1
+    loss = ParallelAnalysis.loss(ctb, plc.τ₁, plc.τ₂, MvNormal([0, 0], [1.0 0.5; 0.5 1.0]))
+    @test loss isa Real
 end
 
 
-# Check type stability
-@code_warntype ParallelAnalysis.contab(obs[:, 1], obs[:, 2])
-@code_warntype ParallelAnalysis.polyc(ParallelAnalysis.contab(obs[:, 1], obs[:, 2]))
-@code_warntype ParallelAnalysis.loss(test, test2.τ₁, test2.τ₂, MvNormal([0, 0], [1.0 0.5; 0.5 1.0]))
-@code_warntype polycor(obs)
 
-test = ParallelAnalysis.contab(obs[:, 1], obs[:, 2])
-test2 = ParallelAnalysis.polyc(test)
-ParallelAnalysis.loss(test, test2.τ₁, test2.τ₂, MvNormal([0, 0], [1.0 0.5; 0.5 1.0]))
-polycor(obs)
 
-# Containing missing response
-o1 = vcat(fill(missing, 10000), obs[:, 1])
-o2 = vcat(obs[:, 2], fill(missing, 10000))
-ParallelAnalysis.contab(o1, o2).X
+# # Check type stability
+# test = ParallelAnalysis.contab(obs[:, 1], obs[:, 2])
+# isapprox(sum(test.X, dims = 2), test.mx)
+# sum(test.X, dims = 1)[:] == test.my
+# @time test2 = ParallelAnalysis.polyc(test)
+# ParallelAnalysis.loss(test, test2.τ₁, test2.τ₂, MvNormal([0, 0], [1.0 0.5; 0.5 1.0]))
+# @profview polycor(obs)
 
-ParallelAnalysis.contab(obs[:, 1], obs[:, 2])
+# @code_warntype ParallelAnalysis.contab(obs[:, 1], obs[:, 2])
+# @code_warntype ParallelAnalysis.polyc(ParallelAnalysis.contab(obs[:, 1], obs[:, 2]))
+# @code_warntype ParallelAnalysis.loss(test, test2.τ₁, test2.τ₂, MvNormal([0, 0], [1.0 0.5; 0.5 1.0]))
+# @code_warntype polycor(obs)
+# @code_warntype ParallelAnalysis.ppdf(MvNormal([0, 0], [1 0.5; 0.5 1]),[0.0, 0.0])
+# @code_warntype ParallelAnalysis.H(-3, -3, 3, 3, MvNormal([0, 0], [1 0.5; 0.5 1]))
+# @profview ParallelAnalysis.H(-3, -3, 3, 3, MvNormal([0, 0], [1 0.5; 0.5 1]))
+
+
+# # Containing missing response
+# o1 = vcat(fill(missing, 10000), obs[:, 1])
+# o2 = vcat(obs[:, 2], fill(missing, 10000))
+# ParallelAnalysis.contab(o1, o2).X
+
+# ParallelAnalysis.contab(obs[:, 1], obs[:, 2])
--- test/Test2_parallel.jl
+++ test/Test2_parallel.jl
@@ -1,3 +1,4 @@
+using Distributions: LinearAlgebra
 using ParallelAnalysis
 using Random, Distributions, LinearAlgebra
 using StatsPlots
@@ -9,16 +10,23 @@ b = [sort(rand(Uniform(-3, 3), 4); rev = false) for i in 1:30]
 
 resp = generate_response(θ, a, b)
 
-fit1 = fa(resp; method = :Polychoric)
-fit2 = fa(resp; method = :Pearson)
-@code_warntype fa(resp; method = :Polychoric)
+@time fit1 = fa(resp; method = :em)
+@time fit2 = fa(resp; cor_method = :Pearson, method = :cm)
+@code_warntype fa(resp; method = :em)
+@profview fa(resp; method = :em)
 
 cov(fit1) |> diagind
 test = cov(fit1)
 test[diagind(test)]
 maximum(test[collect(offdiag(test))])
 
-par_fit = parallel(resp, 10000, x -> fa(x; method = :Polychoric))
-
+par_fit = parallel(resp, 20, x -> fa(x; cor_method = :Polychoric))
+@code_warntype parallel(resp, 10, x -> fa(x; cor_method = :Polychoric))
+@profview parallel(resp, 10, x -> fa(x; cor_method = :Polychoric))
+@code_warntype parallel(resp, 100, x -> fa(x; cor_method = :Pearson))
 
 plot(par_fit)
+
+par_fit = parallel(X, 20, x -> fa(x; cor_method = :Polychoric))
+plot(par_fit)
+par_fit.bounds
